435e1e95348b455d9b95a33e86210d42b940d6ba
Adam Patni <adampatni27@gmail.com>
2020-05-08 18:39:09 -0500

    udated install

--- raspberry-pi/installation_script.sh
+++ raspberry-pi/installation_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd ~
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+sudo apt-get install python3-pip
+pip3 install awscli --upgrade --user
+export PATH=/home/pi/.local/bin:$PATH
 aws configure set aws_access_key_id 'AKIAJRFOLRO6MACCCL6A'
 aws configure set aws_secret_access_key '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'
 aws configure set aws default.region 'us-west-2'
-
