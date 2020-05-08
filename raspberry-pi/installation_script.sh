1f2b7b607545bcd2c573420f6d41da5262a12b77
Adam Patni <adampatni27@gmail.com>
2020-05-08 18:31:00 -0500

    aws installation script

---
+++ raspberry-pi/installation_script.sh
@@ -0,0 +1,8 @@
+cd ~
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws configure set aws_access_key_id 'AKIAJRFOLRO6MACCCL6A'
+aws configure set aws_secret_access_key '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'
+aws configure set aws default.region 'us-west-2'
+
